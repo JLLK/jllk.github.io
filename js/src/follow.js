77a6d16088fcfb2f957e5421435404edf702420f
chentao <chentaov5@gmail.com>
2016-03-18 22:45:36 +0800

    Site updated: 2016-03-18 22:45:36

--- js/src/follow.js
+++ js/src/follow.js
@@ -1,7 +1,7 @@
 //by fleur.wang 
 $(document).ready(function() {
 	var html = '<div id="little_tips" class="follow-tips">'+
-			'<a class="avatar follow-link"><img class="avatar-image" src="http://7xpcru.com1.z0.glb.clouddn.com/jllk_profile.png?imageView2/2/w/76" width="38" height="38" alt=""></a>'+
+			'<a class="avatar follow-link"><img class="avatar-image" src="http://7xpcru.com1.z0.glb.clouddn.com/jllk_github_profile.png?imageView2/2/w/76" width="38" height="38" alt=""></a>'+
 			'<a class="follow-link blog-name-title" href="https://github.com/JLLK" target="new_window"><span class="blog-name">JLLK</span> <span class="blog-title">tomchen</span></a>'+
 			'<a class="follow-button follow-link" target="new_window" href="https://github.com/JLLK">GitHub</a>'+
 			'</div>';
